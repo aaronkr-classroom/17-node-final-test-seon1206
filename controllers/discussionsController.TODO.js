2bf18a062a8008eab18220ae9e0479bfb68fd602
seon1206 <127457404+seon1206@users.noreply.github.com>
2024-06-19 01:43:56 +0000

    기말고사 끝!

--- controllers/discussionsController.TODO.js
+++ controllers/discussionsController.TODO.js
@@ -1,56 +1,152 @@
-// controllers/discussionsController.js
+//discussionsController.js
 "use strict";
 
-const Discussion = require("../models/Discussion"), // 사용자 모델 요청
-  getDiscussionParams = (body, user) => {
-    return {
-      title: body.title,
-      description: body.description,
-      author: user,
-      category: body.category,
-      tags: body.tags,
-    };
+const Discussion = require("../models/Discussion");
+const User = require("../models/User");
+
+const getDiscussionParams = (body, user) => {
+  return {
+    title: body.title,
+    description: body.description,
+    author: user,
+    category: body.category,
+    tags: body.tags,
   };
+};
 
 module.exports = {
-  /**
-   * =====================================================================
-   * C: CREATE / 생성
-   * =====================================================================
-   */
-  // 1. new: 액션,
-  // 2. create: 액션,
-  // 3. redirectView: 액션,
-  /**
-   * =====================================================================
-   * R: READ / 조회
-   * =====================================================================
-   */
-  /**
-   * ------------------------------------
-   * ALL records / 모든 레코드
-   * ------------------------------------
-   */
-  // 4. index: 액션,
-  // 5. indexView: 엑션,
-  /**
-   * ------------------------------------
-   * SINGLE record / 단일 레코드
-   * ------------------------------------
-   */
-  // 6. show: 액션,
-  // 7. showView: 액션,
-  /**
-   * =====================================================================
-   * U: UPDATE / 수정
-   * =====================================================================
-   */
-  // 8. edit: 액션,
-  // 9. update: 액션,
-  /**
-   * =====================================================================
-   * D: DELETE / 삭제
-   * =====================================================================
-   */
-  // 10. delete: 액션,
+  new: (req, res) => {
+    res.render("discussions/new", {
+      page: "new-discussions",
+      title: "New discussions",
+    });
+  },
+
+  create: (req, res, next) => {
+    if (req.skip) next(); // 유효성 체크를 통과하지 못하면 다음 미들웨어 함수로 전달
+
+    let newDiscussion = new Discussion(getDiscussionParams(req.body, req.user)); // 생성된 토론에 현재 사용자 추가
+
+    newDiscussion.save()
+      .then(discussion => {
+        req.flash(
+          "success",
+          `Discussion "${discussion.title}" created successfully!`
+        ); // 플래시 메시지를 추가하고
+        res.locals.redirect = "/discussions"; // 토론 인덱스 페이지로 리디렉션
+        next();
+      })
+      .catch(error => {
+        req.flash(
+          "error",
+          `Failed to create discussion because: ${error.message}.`
+        ); // 에러 메시지를 추가하고
+        res.locals.redirect = "/discussions/new"; // 토론 생성 페이지로 리디렉션
+        next();
+      });
+  },
+
+  redirectView: (req, res, next) => {
+    let redirectPath = res.locals.redirect;
+    if (redirectPath) res.redirect(redirectPath);
+    else next();
+  },
+
+  index: (req, res, next) => {
+    Discussion.find()
+      .populate("author") // author 필드를 populate
+      .populate("users") // users 필드를 populate
+      .exec()
+      .then((discussions) => {
+        res.locals.discussions = discussions;
+        next();
+      })
+      .catch((error) => {
+        console.log(`Error fetching discussions: ${error.message}`);
+        next(error);
+      });
+  },
+
+  indexView: (req, res) => {
+    res.render("discussions/index", {
+      page: "discussions",
+      title: "All discussions",
+    });
+  },
+
+  show: (req, res, next) => {
+    let discussionId = req.params.id;
+    Discussion.findById(discussionId)
+      .populate("author")
+      .populate({
+        path: "comments",
+        populate: {
+          path: "author",
+          model: "User"
+        }
+      })
+      .then((discussion) => {
+        res.locals.discussion = discussion;
+        next();
+      })
+      .catch((error) => {
+        console.log(`Error fetching discussion by ID: ${error.message}`);
+        next(error);
+      });
+  },
+  
+
+  showView: (req, res) => {
+    res.render("discussions/show", {
+      page: "discussion-details",
+      title: "discussion Details",
+    });
+  },
+
+  edit: (req, res, next) => {
+    let discussionId = req.params.id;
+    Discussion.findById(discussionId)
+      .then((discussion) => {
+        res.render("discussion/edit", {
+          discussion: discussion,
+          page: "edit-discussion",
+          title: "Edit discussion",
+        });
+      })
+      .catch((error) => {
+        console.log(`Error fetching discussion by ID: ${error.message}`);
+        next(error);
+      });
+  },
+
+  update: (req, res, next) => {
+    let discussionId = req.params.id,
+      discussionParams = getDiscussionParams(req.body, req.user);
+
+    Discussion.findByIdAndUpdate(discussionId, {
+      $set: discussionParams,
+    })
+      .then((discussion) => {
+        res.locals.redirect = `/discussion/${discussionId}`;
+        res.locals.discussion = discussion;
+        next();
+      })
+      .catch((error) => {
+        console.log(`Error updating discussion by ID: ${error.message}`);
+        next(error);
+      });
+  },
+
+  delete: (req, res, next) => {
+    let discussionId = req.params.id;
+    Discussion.findByIdAndRemove(discussionId)
+      .then(() => {
+        res.locals.redirect = "/discussions";
+        next();
+      })
+      .catch((error) => {
+        console.log(`Error deleting discussion by ID: ${error.message}`);
+        next();
+      });
+  },
 };
--- models/Discussion.js
+++ models/Discussion.js
@@ -1,3 +1,5 @@
+models - Discussion.js
+
 // models/Discussion.js
 "use strict";
 
@@ -22,6 +24,7 @@ const mongoose = require("mongoose"),
       views: { type: Number, default: 0 },
       category: { type: String },
       tags: [String],
+      users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }] 
     },
     {
       timestamps: true,
